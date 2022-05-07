10d82d57567de527651ca0dadc8b4dd6f1ae57eb
Nicolas Alexander Lerch <nicolas.lerch@hm.edu>
2022-05-07 20:05:15 +0200

    functionality finished

--- coding-challenge/src/index.js
+++ coding-challenge/src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+
+
 function Square(props) {
     return (
         <button className="square" onClick={props.onClick}>
